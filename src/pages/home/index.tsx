53f3e02d49429239e34c9d60a81bcf8c17549759
erupturatis <bb.octav@gmail.com>
2023-03-06 20:49:37 +0200

    try static props again

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Husky from '@/components/Husky';
 
-const Home = () => {
+const Home = ({}) => {
     return (
         <div>
             <Husky scale={1} />
         </div>
     );
 };
+
+export async function getStaticProps() {
+    return {};
+}
+
 export default Home;
