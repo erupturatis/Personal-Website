a629fac7643da8e91edfd5866cff39f1ecbb6d43
erupturatis <bb.octav@gmail.com>
2023-03-12 21:02:05 +0200

    working anims middle part

--- src/components/home/middle/Floating.tsx
+++ src/components/home/middle/Floating.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './floating.module.css';
+import anime from 'animejs';
 import { useState, useEffect } from 'react';
 
 type FloatingProps = {
@@ -12,29 +13,56 @@ const Floating = ({ numDivs, height }: FloatingProps) => {
     // generating numDivs divs
     const sWidth = document.querySelector('.FloatingHolder')?.clientWidth;
     function generateDiv(baseSize: number) {
+      const baseAnimationSpeed = 2500;
+
       const div = document.createElement('div');
       let distanceCoefficient = (Math.random() * 0.5 + 0.5) * baseSize;
       distanceCoefficient = Math.floor(distanceCoefficient);
       console.log(distanceCoefficient);
-      div.className = `${styles.morphism} absolute  md:w-[${distanceCoefficient}px] md:h-[${distanceCoefficient}px] `;
+      div.className = `${styles.morphism} absolute  md:w-[${distanceCoefficient}px] md:h-[${distanceCoefficient}px] toAnim`;
       div.style.width = `${distanceCoefficient}px`;
       div.style.height = `${distanceCoefficient}px`;
       div.style.borderRadius = `25%`;
       div.style.marginTop = `${Math.random() * height}px`;
-      div.style.animationDuration = `${(1 / (distanceCoefficient / baseSize)) * 2.5}s`;
+      let animationDuration = (1 / (distanceCoefficient / baseSize)) * baseAnimationSpeed; // we divide by 10 to run the animation faster
       div.style.zIndex = `${distanceCoefficient}`;
+      let initialX;
       if (sWidth) {
-        div.style.transform = `translateX(${Math.random() * sWidth}px)`;
+        initialX = Math.random() * sWidth;
+        div.style.transform = `translateX(${initialX}px)`;
+        let animInitialDuration = (initialX / sWidth) * animationDuration;
+        anime({
+          targets: div,
+          translateX: [initialX, 0],
+          duration: animInitialDuration,
+          easing: 'linear',
+          loop: false,
+          complete: () => {
+            console.log('completed loop');
+            // runs the main animation loop from 0 to Swidth
+            anime({
+              targets: div,
+              translateX: [sWidth, 0],
+              duration: animationDuration,
+              easing: 'linear',
+              loop: true,
+            });
+          },
+        });
       }
 
       const holder = document.querySelector('.FloatingHolder');
       holder?.appendChild(div);
     }
 
-    for (let i = 0; i < numDivs; i++) {
+    for (let i = 0; i < 10; i++) {
       generateDiv(100);
     }
 
+    return () => {};
+  }, []);
+
+  useEffect(() => {
     return () => {};
   }, []);
 
