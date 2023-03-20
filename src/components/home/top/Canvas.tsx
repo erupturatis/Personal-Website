b416c15b83d7a82e4fa9f76d5cd4cd17dde7b19c
erupturatis <bb.octav@gmail.com>
2023-03-20 14:50:15 +0200

    calc added

--- src/components/home/top/Canvas.tsx
+++ src/components/home/top/Canvas.tsx
@@ -107,8 +107,8 @@ const Canvas = ({ x1, y1, x2, y2, widthP, heightP, accent, offset }: CanvasProps
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           // Run your async JavaScript code here
-          // scrollCalculator();
-          // startAnimation();
+          scrollCalculator();
+          startAnimation();
         } else {
           if (listener) window.removeEventListener('scroll', listener);
           // stops animation
