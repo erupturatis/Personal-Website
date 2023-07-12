4a339f038791e1559461a1f6ec1c5d28df695436
erupturatis <bb.octav@gmail.com>
2023-07-12 09:50:11 +0300

    (refactor)[navbar]

--- src/components/navbar/ButtonDesktop.tsx
+++ src/components/navbar/ButtonDesktop.tsx
@@ -15,14 +15,17 @@ type navButtonProps = {
 };
 
 const ButtonDesktop = ({ text, link }: navButtonProps) => {
+  const src = '/winter-theme/snow-under-button.png';
   return (
-    <Link href={link}>
-      <div
-        className={` ${kanit.className} py-2 text-xl first-letter:select-none  cursor-pointer transition-all text-white  opacity-80 hover:opacity-100 ease-in-200`}
-      >
-        <div>{text}</div>
-      </div>
-    </Link>
+    <>
+      <Link href={link}>
+        <div
+          className={` ${kanit.className} relative py-2 text-xl first-letter:select-none  cursor-pointer transition-all text-white hover:text-blue-300  duration-500  opacity-80 hover:opacity-100 ease-in-200`}
+        >
+          <div className={'text-center'}>{text}</div>
+        </div>
+      </Link>
+    </>
   );
 };
 
--- src/components/navbar/Desktop.tsx
+++ src/components/navbar/Desktop.tsx
@@ -5,12 +5,14 @@ import Icecycle from '@components/navbar/Icecycle';
 const Desktop = () => {
   return (
     <div>
-      <div className='flex justify-center items-center h-20 z-20 w-full gap-20 group '>
-        <Icecycle inverse={false} />
-        <ButtonDesktop inverse={false} text='Home' link='/home' />
-        <ButtonDesktop inverse={true} text='Contact' link='/contact' />
-        <ButtonDesktop inverse={false} text='Projects' link='/projects' />
-        <Icecycle inverse={true} />
+      <div className='flex justify-center items-center mt-6 z-20 w-full  gap-20 group '>
+        <div className={' flex justify-center gap-16 items-center'}>
+          <Icecycle inverse={false} />
+          <ButtonDesktop inverse={false} text='Home' link='/home' />
+          <ButtonDesktop inverse={true} text='Contact' link='/contact' />
+          <ButtonDesktop inverse={false} text='Projects' link='/projects' />
+          <Icecycle inverse={true} />
+        </div>
       </div>
     </div>
   );
