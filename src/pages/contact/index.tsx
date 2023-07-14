68b370edd547dc77244abf0a735d40d690402ff5
erupturatis <bb.octav@gmail.com>
2023-07-14 20:13:52 +0300

    (create)[seo for contact]

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
+import Head from 'next/head';
 
 const montserrat = Montserrat({
   weight: ['300', '400', '500', '600', '700'],
@@ -26,34 +27,42 @@ const Button = ({ src, alt }: IButtonProps) => {
 const Contact = () => {
   const src = '/winter-theme/blueice.jpg';
   return (
-    <div className={'w-full mt-40'}>
-      <div
-        className={
-          'w-full text-center text-white  text-4xl ' + montserrat.className
-        }
-      >
-        Contact me
-      </div>
-      <div className={'w-full flex justify-center mt-10'}>
-        <div className={'rounded-full w-96 h-96 '}>
-          <Image
-            src={src}
-            alt='picture of the skill'
-            width={0}
-            height={0}
-            sizes='100vw'
-            style={{ width: '100%', height: '100%', borderRadius: '100%' }} // optional
-          />
+    <>
+      <Head>
+        <title>Contact</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content='List of ways to contact me' />
+      </Head>
+
+      <div className={'w-full mt-40'}>
+        <div
+          className={
+            'w-full text-center text-white  text-4xl ' + montserrat.className
+          }
+        >
+          Contact me
+        </div>
+        <div className={'w-full flex justify-center mt-10'}>
+          <div className={'rounded-full w-96 h-96 '}>
+            <Image
+              src={src}
+              alt='picture of the skill'
+              width={0}
+              height={0}
+              sizes='100vw'
+              style={{ width: '100%', height: '100%', borderRadius: '100%' }} // optional
+            />
+          </div>
+        </div>
+
+        <div className={'flex justify-center gap-10 mt-20'}>
+          <Button src='/socials/linkedin.png' alt='linkedin' />
+          <Button src='/socials/linkedin.png' alt='linkedin' />
+          <Button src='/socials/linkedin.png' alt='linkedin' />
+          <Button src='/socials/linkedin.png' alt='linkedin' />
         </div>
       </div>
-
-      <div className={'flex justify-center gap-10 mt-20'}>
-        <Button src='/socials/linkedin.png' alt='linkedin' />
-        <Button src='/socials/linkedin.png' alt='linkedin' />
-        <Button src='/socials/linkedin.png' alt='linkedin' />
-        <Button src='/socials/linkedin.png' alt='linkedin' />
-      </div>
-    </div>
+    </>
   );
 };
 
